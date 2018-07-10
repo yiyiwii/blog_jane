392f9eb8afbef2c8cd6c563081eb991ca2ae6a3b
yiyiwii <dongh.tsai@gmail.com>
2018-07-09 23:19:00 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
 # Add changes to git.
-git add .
+git add -A
 
 # Commit changes.
 msg="rebuilding site `date`"
